e4dd4c21c34a51d10922de739be308a9f8c8bf8e
Gabriel Cari <g.cari@hotmail.com>
2023-03-17 19:00:12 -0300

    added 04-api-le-voucher: createVoucher

---
+++ 04-api-le-voucher/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: [
+    "<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
+  ],
+  // testMatch: ["<rootDir>/**/tests/*.(test|spec).ts"],
+};
---
+++ 04-api-le-voucher/src/tests/unit/voucherService.test.ts
@@ -0,0 +1,55 @@
+import voucherService from "services/voucherService";
+import voucherRepository from "repositories/voucherRepository";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+});
+
+describe("createVoucher test suite", () => {
+  it("should throw error on create voucher when it already exist", () => {
+    const code = "unique code";
+    const discount = 20;
+    const voucher = {
+      id: 1,
+      code,
+      discount,
+      used: false
+    };
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => {
+      return { ...voucher };
+    });
+    jest.spyOn(voucherRepository, "createVoucher");
+    const promise = voucherService.createVoucher(code, discount);
+    expect(promise).rejects.toHaveProperty("message");
+    expect(promise).rejects.toHaveProperty("type");
+    expect(voucherRepository.createVoucher).not.toBeCalled();
+  });
+
+  it("should create voucher when passing non existent code and discount", async () => {
+    const code = "unique code";
+    const discount = 20;
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce(() => undefined);
+    jest.spyOn(voucherRepository, "createVoucher").mockImplementationOnce(() => undefined);
+    await voucherService.createVoucher(code, discount);
+    expect(voucherRepository.createVoucher).toBeCalled();
+  });
+});
+
+describe("applyVoucher test suite", () => {
+  it("should throw error when not finding voucher", () => {
+    const code = "unique code";
+    const amount = 30;
+    const voucher = {
+      id: 1,
+      code,
+      discount: amount,
+      used: false
+    };
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => undefined);
+    const promise = voucherService.applyVoucher(code, amount);
+    expect(promise).rejects.toHaveProperty("message");
+    expect(promise).rejects.toHaveProperty("type");
+  });
+});
