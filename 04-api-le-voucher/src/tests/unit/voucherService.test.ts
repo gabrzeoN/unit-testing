55165d3e5802ae6dca9a5a018275530f9d7f0661
Gabriel Cari <g.cari@hotmail.com>
2023-03-17 19:15:12 -0300

    (04): applyVoucher

--- 04-api-le-voucher/src/tests/unit/voucherService.test.ts
+++ 04-api-le-voucher/src/tests/unit/voucherService.test.ts
@@ -41,15 +41,42 @@ describe("applyVoucher test suite", () => {
   it("should throw error when not finding voucher", () => {
     const code = "unique code";
     const amount = 30;
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => undefined);
+    const promise = voucherService.applyVoucher(code, amount);
+    expect(promise).rejects.toHaveProperty("message");
+    expect(promise).rejects.toHaveProperty("type");
+  });
+
+  it("should not apply voucher on amount under 100", () => {
+    const code = "unique code";
+    const amount = 30;
     const voucher = {
       id: 1,
       code,
       discount: amount,
       used: false
     };
-    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => undefined);
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => voucher);
+    jest.spyOn(voucherRepository, "useVoucher").mockImplementationOnce((): any => undefined);
     const promise = voucherService.applyVoucher(code, amount);
-    expect(promise).rejects.toHaveProperty("message");
-    expect(promise).rejects.toHaveProperty("type");
+    expect(promise).resolves.toHaveProperty("amount", amount);
+    expect(promise).resolves.toHaveProperty("finalAmount", amount);
+    expect(promise).resolves.toHaveProperty("applied", false);
+  });
+
+  it("should apply voucher on amount over 100", () => {
+    const code = "unique code";
+    const amount = 100;
+    const voucher = {
+      id: 1,
+      code,
+      discount: amount,
+      used: false
+    };
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => voucher);
+    jest.spyOn(voucherRepository, "useVoucher").mockImplementationOnce((): any => undefined);
+    const promise = voucherService.applyVoucher(code, amount);
+    expect(promise).resolves.toHaveProperty("amount", amount);
+    expect(promise).resolves.toHaveProperty("applied", true);
   });
 });
